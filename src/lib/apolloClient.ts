554bfec92de53629a49955eb1fa5de1e7cd16661
Anuoluwapo Pelumi Hammed <hammedanuoluwapopelumi@gmail.com>
2025-10-17 20:08:30 +0000

    feat: add Apollo Client configuration
    - Create Apollo Client instance with HTTP link to GraphQL API
    - Add authentication link for Bearer token from localStorage
    - Configure InMemoryCache with cache-and-network fetch policy

---
+++ src/lib/apolloClient.ts
@@ -0,0 +1,29 @@
+import { ApolloClient, InMemoryCache, HttpLink } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+
+const httpLink = new HttpLink({
+  uri: 'https://proud-jobyna-campusuniverse-22c402c3.koyeb.app/api/graphql',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('accessToken');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    }
+  };
+});
+
+export const apolloClient = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+  defaultOptions: {
+    watchQuery: {
+      fetchPolicy: 'cache-and-network',
+    },
+  },
+});
