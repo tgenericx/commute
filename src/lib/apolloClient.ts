2cdc5b0b30b56a048f73bd82841defe3dcedc532
Anuoluwapo Pelumi Hammed <hammedanuoluwapopelumi@gmail.com>
2025-10-18 23:16:56 +0000

    refactor: update Apollo Client configuration
    - Replace deprecated `setContext` function with `SetContextLink` class
    - Remove default options configuration to use Apollo's defaults
    - Maintain authentication header functionality with token from localStorage
    - Simplify client setup while preserving existing behavior

--- src/lib/apolloClient.ts
+++ src/lib/apolloClient.ts
@@ -1,5 +1,5 @@
 import { ApolloClient, InMemoryCache, HttpLink } from '@apollo/client';
-import { setContext } from '@apollo/client/link/context';
+import { SetContextLink } from "@apollo/client/link/context";
 
 const httpLink = new HttpLink({
   uri: 'https://proud-jobyna-campusuniverse-22c402c3.koyeb.app/api/graphql',
@@ -8,7 +8,7 @@ const httpLink = new HttpLink({
   },
 });
 
-const authLink = setContext((_, { headers }) => {
+const authLink = new SetContextLink(({ headers }) => {
   const token = localStorage.getItem('accessToken');
   return {
     headers: {
@@ -21,9 +21,4 @@ const authLink = setContext((_, { headers }) => {
 export const apolloClient = new ApolloClient({
   link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
-  defaultOptions: {
-    watchQuery: {
-      fetchPolicy: 'cache-and-network',
-    },
-  },
 });
