bcb888576b56978d7d4322c897988dae83b86c3b
Anuoluwapo Pelumi Hammed <hammedanuoluwapopelumi@gmail.com>
2025-10-23 13:07:51 +0000

    remove speed insight

--- src/main.tsx
+++ src/main.tsx
@@ -4,12 +4,10 @@ import { ApolloProvider } from '@apollo/client/react';
 import './index.css'
 import App from './App.tsx'
 import { apolloClient } from './lib/apolloClient';
-import { SpeedInsights } from "@vercel/speed-insights/react"
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ApolloProvider client={apolloClient}>
-      <SpeedInsights />
       <App />
     </ApolloProvider>
   </StrictMode>,
