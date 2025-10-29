fd4f03551d3a3f4dd96b1e4790f99f3ea8f40325
root <root@localhost.localdomain>
2025-10-29 11:22:36 +0000

    removed unused input

--- src/components/post/create-form.tsx
+++ src/components/post/create-form.tsx
@@ -2,7 +2,6 @@ import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { z } from "zod";
 import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
 import { useMutation } from "@apollo/client/react";
 import {
   CreatePostDocument,
