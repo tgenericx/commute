0f24783818011c821d5f67195cbea89160a37b7a
root <root@localhost.localdomain>
2025-10-30 16:02:10 +0000

    add base post action props to post types

--- src/components/post/types.ts
+++ src/components/post/types.ts
@@ -4,3 +4,9 @@ export const adaptPostMedia = (postMedia?: (PostMedia | null)[]): Media[] =>
   (postMedia ?? [])
     .filter((pm): pm is PostMedia & { media: Media } => !!pm?.media)
     .map(({ media }) => media);
+
+export interface BaseActionProps {
+  count?: number;
+  onClick?: (e?: React.MouseEvent<HTMLButtonElement>) => void;
+  className?: string;
+}
